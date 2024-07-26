718ab803b18caf54e08de5f30a04e4bda2ba2044
Benjamín Peyraga <benjapey99@gmail.com>
2024-07-26 16:23:19 -0300

    Chat ya acasi listo.

--- src/app.js
+++ src/app.js
@@ -1,28 +1,51 @@
-import express from 'express'
-import handlebars from 'express-handlebars'
-import { __dirname } from './utils.js'
+import express from 'express';
+import handlebars from 'express-handlebars';
+import { __dirname } from './utils.js';
+import { Server } from 'socket.io';
+//  Rutas
+import chatRoute from './routes/chat.route.js'
 
 
 const app = express();
 const PORT = 8080 || 3000;
 
+//  Motor de plantillas Express-Handlebars.
 app.engine('handlebars', handlebars.engine());
 app.set('views', __dirname + '/views');
 app.set('view engine', 'handlebars');
-
+// Middleware para utilizacion de jsony formualrios.
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+// Carpeta Public.
 app.use(express.static(__dirname + '/public'));
 
-app.get("/", (req, res) => {
-    res.render("chatbox", {
-        name: "Benjamin"
-    })
-});
+//  Rutas
+app.use('/chat', chatRoute);
 
+const conversacion = [];
+/// Extraer todos los nombres de usuario
+const allUsers = conversacion.map(item => item.user);
 
+// Crear un conjunto para eliminar duplicados
+const uniqueUsers = new Set(allUsers);
 
-app.listen(PORT, () => {
+// Convertir el conjunto de nuevo a un array
+const usuariosUnicos = Array.from(uniqueUsers);
+
+const httpServer = app.listen(PORT, () => {
     console.log("Server ON")
 });
+const io = new Server(httpServer);
+
+io.on('connection', (socket) => {
+    console.log("Dispositivos conectados: " + io.engine.clientsCount);
+
+    socket.on('mensaje', (data) => {
+        conversacion.push(data);
+        io.emit('conversacion', conversacion);
+        io.emit('userON', usuariosUnicos);
+    });
+    io.emit('conversacion', conversacion);
+    io.emit('userON', usuariosUnicos);
+});
+
--- src/public/js/chatbox.js
+++ src/public/js/chatbox.js
@@ -1,1 +1,97 @@
-console.log("Entrando al cliente");
+const socket = io();
+const title = document.querySelector("#title");
+let user = '';
+
+Swal.fire({
+    title: 'Ingresar Nickname',
+    input: 'text',
+    inputPlaceholder: 'Ingrese su Nickname',
+    showCancelButton: true,
+    confirmButtonText: 'Enviar',
+    cancelButtonText: 'Cancelar',
+    allowOutsideClick: false,
+    inputValidator: (value) => {
+        if (!value) {
+            return '¡Debe ingresar un Nickname!';
+        }
+    }
+}).then((result) => {
+    if (result.isConfirmed) {
+        user = result.value;
+        Swal.fire({
+            title: 'Nickname ingresado',
+            text: user,
+            timer: 500, // Duración en milisegundos
+            showConfirmButton: false // Opcional: oculta el botón de confirmación
+        });
+        title.innerText = `Bienvenido al Chat Grupal, ${user}!`;
+    } else {
+        user = "Anonimo";
+    }
+    socket.emit('newUser', { user });
+});
+
+/*
+// Lista de usuarios en UsersBox.
+const usersBox = document.querySelector("#usersBox");
+io.on("users", (data) => {
+    const ul = document.createElement('ul');
+    data.foreach((user) => {
+        const li = document.createElement('li');
+        li.innerText = user;
+        ul.appendChild(li)
+    })
+})
+    */
+const inputChatBox = document.querySelector("#inputMensajes");
+
+inputChatBox.addEventListener('keyup', (event) => {
+    if (event.key == 'Enter') {
+        const id = new Date();
+        const fecha = id.toLocaleDateString(); // Obtener la fecha en formato local
+        const hora = String(id.getHours()).padStart(2, '0'); // Obtener la hora en formato local
+        const min = String(id.getMinutes()).padStart(2, '0');
+        socket.emit('mensaje', { user, mensaje: event.target.value, fecha: fecha, hora: hora, min: min });
+        inputChatBox.value = '';
+    }
+});
+
+
+socket.on('conversacion', (data) => {
+    const chatBox = document.querySelector("#mensajes");
+    chatBox.innerHTML = '';
+    data.forEach(element => {
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <h4>${element.user}</h4>
+        <p>${element.mensaje}</p>
+        <p class="horario-mensaje"><span>${element.hora}</span>:<span>${element.min}</span></p>`
+        chatBox.appendChild(div);
+    });
+    
+});
+
+socket.on('userON', (data)=>{
+    const usersON = document.querySelector("#usersON");
+    const ul = document.createElement('ul');
+    ul.innerHTML = '';
+    usuariosUnicos.forEach(element => {
+        const li = document.createElement('li');
+        li.innerHTML = element;
+        ul.appendChild(li);
+    })
+    usersON.appendChild(ul)
+})
+
+/*
+/// Extraer todos los nombres de usuario
+const allUsers = data.map(item => item.user);
+
+// Crear un conjunto para eliminar duplicados
+const uniqueUsers = new Set(allUsers);
+
+// Convertir el conjunto de nuevo a un array
+const usuariosUnicos = Array.from(uniqueUsers);
+
+
+*/
